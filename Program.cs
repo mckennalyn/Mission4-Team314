19c7479eb9b1e3045d5ae589a574fcd313450ac3
mckennalyn <131218663+mckennalyn@users.noreply.github.com>
2024-01-29 17:11:23 -0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Tic-Tac-Toe!");
+        char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        char currentPlayer = 'X';
+        Supporting game = new Supporting();
+
+        while (true)
+        {
+            Console.Clear();
+            game.PrintBoard(board);
+            Console.Write($"Player {currentPlayer}, enter your move (1-9): ");
+            int move = int.Parse(Console.ReadLine());
+
+            if (move < 1 || move > 9 || board[move - 1] == 'X' || board[move - 1] == 'O')
+            {
+                Console.WriteLine("Invalid move. Try again.");
+                continue;
+            }
+
+            board[move - 1] = currentPlayer;
+
+            if (game.CheckForWin(board, currentPlayer))
+            {
+                Console.Clear();
+                game.PrintBoard(board);
+                Console.WriteLine($"Player {currentPlayer} wins!");
+                break;
+            }
+
+            if (Array.IndexOf(board, ' ') == -1)
+            {
+                Console.Clear();
+                game.PrintBoard(board);
+                Console.WriteLine("It's a draw!");
+                break;
+            }
+
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        }
+    }
+}
